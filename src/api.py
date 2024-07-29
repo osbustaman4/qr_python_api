5c927fd0fb545d8114ed15efef76d75406e432ca
osbustaman4 <osvaldo.bustamante.n@gmail.com>
2024-07-29 09:57:58 -0400

    upload pendiente

--- src/api.py
+++ src/api.py
@@ -1,12 +1,15 @@
+# Importamos las librerías necesarias.
+
+import cv2
+import numpy as np
 import base64
 import os
 import tempfile
 import traceback
 
-import cv2  # OpenCV para manipulación de imágenes
-from pyzbar.pyzbar import decode  # pyzbar para decodificar códigos QR
+from pyzbar import pyzbar
 from PIL import Image  # Pillow para manipulación de imágenes
-
+from argparse import ArgumentParser
 from datetime import date, datetime, timedelta
 from flask import Blueprint, request, jsonify, send_file, Response
 
@@ -37,12 +40,18 @@ def read_image_code():
             temp.write(image_decode)
             temp_path = temp.name
 
-        # Leer el código QR de la imagen temporal
-        qr_result = leer_qr(temp_path)
+        # Leemos la imagen de entrada.
+        image = cv2.imread(temp_path)
 
-        if not qr_result["success"]:
-            return Utils.create_response(qr_result["message"], False, 404)
-        
+        # Preprocesamos la imagen para mejorar la detección.
+        preprocessed_image = preprocess_image(image)
+
+        # Extraemos los códigos de barra de la imagen.
+        barcodes = detect_and_decode(preprocessed_image)
+
+        # Dibujamos los códigos de barra/QR en la imagen original.
+        qr_result = draw_barcodes(image, barcodes)
+
         # Cuando hayas terminado de usar el archivo temporal, puedes eliminarlo así:
         os.remove(temp_path)
         
@@ -61,47 +70,81 @@ def read_image_code():
         return Utils.create_response(message, False, 500)
     
 
-
-# Función para ajustar la imagen antes de decodificar el código QR
-def procesar_imagen(ruta_imagen):
-    # Leer la imagen
-    imagen = cv2.imread(ruta_imagen)
+def preprocess_image(image):
+    """
+    Apply preprocessing to the image to improve barcode/QR code detection.
+    Convert the image to grayscale, adjust contrast, and apply rotation correction.
+    """
+    # Convertimos a escala de grises
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    # Convertir a escala de grises
-    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    # Ajustamos el contraste y brillo (puedes ajustar estos valores)
+    alpha = 1.5  # Contraste
+    beta = 0.5     # Brillo
+    contrast_adjusted = cv2.convertScaleAbs(gray, alpha=alpha, beta=beta)
     
-    # Aplicar filtro de desenfoque para reducir el ruido
-    desenfoque = cv2.GaussianBlur(gris, (5, 5), 0)
+    # Aplicamos un filtro de suavizado para reducir el ruido
+    blurred = cv2.GaussianBlur(contrast_adjusted, (5, 5), 0)
     
-    # Aplicar umbral para mejorar el contraste
-    _, umbralizada = cv2.threshold(desenfoque, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # Realizamos la binarización de la imagen
+    _, binary = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     
-    return umbralizada
-
-
-# Función para decodificar un código QR desde una imagen procesada
-def leer_qr(ruta_imagen):
-    # Procesar la imagen
-    imagen_procesada = procesar_imagen(ruta_imagen)
+    # Detectar y corregir la inclinación
+    binary = correct_rotation(binary)
     
-    # Decodificar el código QR en la imagen
-    decodificaciones = decode(imagen_procesada)
+    return binary
+
+def correct_rotation(image):
+    """
+    Detect and correct rotation of the image.
+    """
+    # Detectar bordes en la imagen
+    edges = cv2.Canny(image, 60, 150, apertureSize=3)
+    print(edges)
     
-    # Si se encontró al menos un código QR
-    if decodificaciones:
-        # Retornar el dato decodificado del primer código QR encontrado
-        return {
-            "success": True,
-            "message": decodificaciones[0].data.decode('utf-8')
-        }
-    else:
-        # Si no se encontró ningún código QR
-        return {
-            "success": False,
-            "message": "No se encontró ningún código QR en la imagen"
-        }
+    # Detectar líneas en la imagen
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+    angle = 0
+    
+    if lines is not None:
+        # Calcular el ángulo promedio de las líneas detectadas
+        angles = [np.degrees(np.arctan2(line[0][1], line[0][0])) for line in lines]
+        angle = np.mean(angles)
+    
+    # Rotar la imagen para corregir la inclinación
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    
+    return rotated
 
+def detect_and_decode(image):
+    """
+    Detect and decode barcodes/QR codes in the given image.
+    """
+    barcodes = pyzbar.decode(image)
+    return barcodes
 
+def draw_barcodes(image, barcodes):
+    """
+    Draw rectangles and add text labels on the detected barcodes/QR codes.
+    """
+    code_response = []
 
+    for i, barcode in enumerate(barcodes, start=1):
+        x, y, width, height = barcode.rect
+        cv2.rectangle(image, (x, y), (x + width, y + height), (0, 0, 255), 2)
+        
+        data = barcode.data.decode('utf-8')
+        type_ = barcode.type
+        
+        text = f'{data} ({type_})'
+        code_response.append({
+            "data": data,
+            "type": type_
+        })
+        cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 2)
 
+    return code_response
 
